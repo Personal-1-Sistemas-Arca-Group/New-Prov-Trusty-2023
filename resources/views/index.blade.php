ecf626b0798d2168aca05085dfdd02e3bfffd54d
Perosnal_1_Sistemas_Arca_Group <sistemasagrh@gmail.com>
2023-02-01 10:39:03 -0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="shortcut icon" href="img/Logo_Prov_Trusty.png" type="image/x-icon">
+    <script src="js/index.js"></script>
+    <title>Prov-Trusty</title>
+</head>
+
+<body>
+    <header>
+        <a href="" class="box">
+            <i class="fas fa-home"></i>
+        </a>
+    </header>
+    <div class="content">
+        <div class="caja-semaforo">
+            <div class="box">
+                <div class="traffic-light ">
+                    <div class="box-traffic-light">
+                        <div class="circle dright-red">
+                            <div class="circle-inside red"></div>
+                        </div>
+                        <div class="circle dright-yellow">
+                            <div class="circle-inside yellow"></div>
+                        </div>
+                        <div class="circle dright-green">
+                            <div class="circle-inside green"></div>
+                        </div>
+                    </div>
+                    <div class="tube"></div>
+                </div>
+                <div class="info">
+                    <div class="box-info red">
+                        <h4 class="dright-red">NO PASA</h4>
+                        <p>
+                            Goza de una integridad pobre, tiende a encubrir actos deshonestos y a buscar la forma de
+                            justificarlos, su actuar normalmente es deshonesto.
+                        </p>
+                    </div>
+                    <div class="box-info yellow">
+                        <h4 class="dright-yellow">PASA CON PRECAUCIÓN</h4>
+                        <p>
+                            Goza de una integridad común, tiende a encubrir actos deshonestos cuando considera que las
+                            situaciones lo justifican.
+                        </p>
+                    </div>
+                    <div class="box-info green">
+                        <h4 class="dright-green">PASA</h4>
+                        <p>
+                            Poseé un sentido de honestidad exepcional, así que guía sus acciones en valores y principios
+                            sólidos.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="caja-index">
+            <div class="bar white"></div>
+            <div class="bar white"></div>
+            <div class="login">
+                <div class="box-img">
+                    <img src="img/Logo_Prov_Trusty.png" alt="" width="100%" srcset="">
+                </div>
+                <div class="box-form">
+                    <form action="" method="post">
+                        <input type="text" name="" id="" placeholder="Nombre Usuario*">
+                        <input type="password" name="" id="" placeholder="Contraseña*">
+                        <button>INGRESAR</button>    
+                    </form>
+                </div>
+            </div>
+            <div class="bar white"></div>
+            <div class="bar white"></div>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/a9e19848a9.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
